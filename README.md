f7ef7a511b0b920ed6adeb4af5734755eb934a7d
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-11-15 12:49:44 -0700

    updated readme + made poll_id

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Live-Poll
+
+## Set Up
+
+1 clone repository
+2 run npm install
+3 un comment /index.js 14 // createNewPoll();, save
+4 run npm run dev
+5 change /public/sketch.js 7 let poll_id = "changeThis" 
+
+to the one found in database.db
--- public/sketch.js
+++ public/sketch.js
@@ -4,10 +4,11 @@
 let poll = {};
 const maxEmojis = 40;
 let voteButton;
+let poll_id = "Change this to your poll id"
 
 async function countVotes() {
   // TODO this page should be for a specific poll
-  const response = await fetch("/poll/6pKgCWCV06Rp2rF5");
+  const response = await fetch(`/poll/${poll_id}`);
   poll = await response.json();
   if (poll.message) throw new Error(poll.message)
   return poll
@@ -31,7 +32,7 @@ async function submitVote() {
   let choice = radio.value(); // choice is a number
   // TODO: select poll id somehow => URL query parameters?
   if (!isNaN(choice)) {
-    let response = await fetch(`vote/6pKgCWCV06Rp2rF5/${choice}`);
+    let response = await fetch(`vote/${poll_id}/${choice}`);
     let status = await response.json();
     console.log(status);
   } else {
