0cb26751ca1870fdd42c2c763395fad5d73e27ae
D-T-666 <tabatadzedima20@gmail.com>
2020-11-23 20:51:23 +0400

    minor restructure and refactoring

---
+++ public/script.js
@@ -0,0 +1,49 @@
+// Function currently not in use
+function getPollID() {
+  const { pollId } = getURLParams();
+  // TODO: instead of default poll add a separate page for user to input poll id?
+  if (!pollId) {
+    if (poll_id == undefined)
+      poll_id = prompt('Poll id') || default_poll_id;
+    return poll_id;
+  }
+  return pollId;
+}
+
+function getPollID() {
+  const { pollId } = getURLParams();
+  // TODO: instead of default poll add a separate page for user to input poll id?
+  if (!pollId) return default_poll_id;
+  return pollId;
+}
+
+async function countVotes() {
+  // TODO: this page should be for a specific poll
+
+  // Get the poll id
+  poll_id = getPollID();
+
+  // Fetch the poll
+  const response = await fetch(`/poll/${poll_id}`);
+  // Extract the json
+  poll = await response.json();
+
+  // Throw an error if the poll has an error message
+  if (poll.message)
+    throw new Error(poll.message);
+
+  // After all, display the results
+  displayResults(poll);
+}
+
+async function submitVote() {
+  let choice = radio.value(); // Choice is a number
+
+  if (choice) {
+    let response = await fetch(`vote/${poll_id}/${choice}`);
+    let status = await response.json();
+    console.log(status);
+  } else {
+    console.log('no choice selected');
+  }
+}
--- public/sketch.js
+++ public/sketch.js
@@ -1,115 +1,16 @@
-// TODO: create a poll creation page
-// Different pages for voting, viewing, and poll creation
+// TODO: create different pages for voting, viewing, and poll creation
 
 let poll = {};
-let poll_id = undefined; // copy the _id from database.db
-const default_poll_id = 'p7PqWACbsGVnSMQK'
-const maxEmojis = 40;
+let poll_id = undefined; // Copy the _id from database.db
+const default_poll_id = 'p7PqWACbsGVnSMQK';
 let voteButton;
-<<<<<<< HEAD
-<<<<<<< HEAD
 let trainEngin;
 let trainPart;
-=======
-let poll_id = 'Xyk1rXxIbDfFA0hy';
->>>>>>> b2df93c... some cleanup in sketch.js
-
-function preload(){
-  trainEngin = loadImage('assets/engin.png')
-  trainPart = loadImage('assets/part.png')
-}
-
-// function currently not in use
-function getPollID() {
-<<<<<<< HEAD
-  const { pollId } = getURLParams();
-  // TODO: instead of default poll add a separate page for user to input poll id?
-  if (!pollId) {
-		if (poll_id == undefined)
-			poll_id = prompt('Poll id') || default_poll_id
-		return poll_id;
-	};
-	return pollId;
-}
-=======
-const default_poll_id = 'Xyk1rXxIbDfFA0hy';
-<<<<<<< HEAD
->>>>>>> ca7cd40... some minor poll_id refactor
-=======
-let poll_id = undefined;
->>>>>>> 27cee40... fixed poll-id
-
-function getPollID() {
-  const { pollId } = getURLParams();
-  // TODO: instead of default poll add a separate page for user to input poll id?
-  if(!pollId) return '1m325LWz9t1eX2fT'
-  return pollId
-=======
-	const { pollId } = getURLParams();
-	// TODO: instead of default poll add a separate page for user to input poll id?
-	if (!pollId) {
-		if (poll_id == undefined)
-			poll_id = prompt('Poll id') || default_poll_id
-		return poll_id;
-	};
-	return pollId;
->>>>>>> b2df93c... some cleanup in sketch.js
-}
-
 let firstRender = true;
 
-async function countVotes() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  // TODO this page should be for a specific poll
-  poll_id = getPollID();
-  const response = await fetch(`/poll/${poll_id}`);
-  poll = await response.json();
-  if (poll.message) {
-    throw new Error(poll.message);
-  }
-  displayResults(poll);
-}
-
-function displayResults(poll) {
-
-  let resultsDiv = document.getElementById('results');
-
-  let maxVotes = 0;
-  let totalVotes = 0;
-  for (let count of poll.votes) {
-    maxVotes = max(count, maxVotes);
-    totalVotes += count;
-  }
-
-  for(const [index, option] of Object.entries(poll.options)) {
-    let count = poll.votes[index];
-    let width = map(count, 0, maxVotes, 0, 100);
-    if (firstRender) {
-      //Container div per option
-      let optionDiv = document.createElement('div');
-      optionDiv.classList.add('option');
-      // Add option text on a separate line, so it can get any length
-      let optionText = document.createElement('span');
-      optionText.innerHTML = option;
-      optionDiv.append(optionText);
-      resultsDiv.append(optionDiv);
-
-      //Add option progress bar
-      let progressBar = document.createElement('div');
-      progressBar.id = 'progressBar_' + index;
-      progressBar.classList.add('progressBar');
-      progressBar.style.setProperty('width', width + '%');
-      progressBar.innerHTML = Math.round(count/totalVotes*100) + '%';
-      resultsDiv.append(progressBar);
-    } else {
-      let progressBar = document.getElementById("progressBar_" + index);
-      progressBar.style.setProperty('width', width + '%');
-      progressBar.innerHTML = Math.round(count/totalVotes*100) + '%';
-    }
-  }
-
-  firstRender = false;
+function preload() {
+  trainEngin = loadImage('assets/engin.png');
+  trainPart = loadImage('assets/part.png');
 }
 
 async function setup() {
@@ -117,107 +18,65 @@ async function setup() {
   await countVotes();
   setInterval(countVotes, 500);
 
-  let pollQ = createElement('p', poll.question)
-  pollQ.addClass("question")
+  let pollQ = createElement('p', poll.question);
+  pollQ.addClass("question");
 
   radio = createRadio();
   for (let i = 0; i < poll.options.length; i++) {
-    radio.option(i, poll.options[i]) // First arg is index, second arg is what is visible to user 
+    radio.option(i, poll.options[i]); // First arg is index, second arg is what is visible to user 
   }
   // radio.style('width', '50px'); // Change this for width of radio 
+
+  // Create the vote button
   voteButton = createButton('vote');
-  voteButton.addClass("VoteBTN")
+  // And add a class and a mouse pressed event listener
+  voteButton.addClass("VoteBTN");
   voteButton.mousePressed(submitVote);
 }
 
-async function submitVote() {
-  let choice = radio.value(); // choice is a number
-  if (!isNaN(choice)) {
-    let response = await fetch(`vote/${poll_id}/${choice}`);
-    let status = await response.json();
-    console.log(status);
-  } else {
-    console.log('no choice selected');
+function displayResults(poll) {
+  // Create 
+  let resultsDiv = document.getElementById('results');
+
+  // Get the number of votes that the most voted option has.
+  let maxVotes = poll.votes.reduce((a, b) => (a > b ? a : b));
+
+  // Get the total number of votes.
+  let totalVotes = poll.votes.reduce((a, b) => (a + b));
+
+  for (const [index, option] of Object.entries(poll.options)) {
+    let count = poll.votes[index];
+    let width = map(count, 0, maxVotes, 0, 100);
+
+    if (firstRender)
+      doTheFirstRender(index, option, resultsDiv);
+
+    // Get the progress bar element
+    let progressBar = document.getElementById("progressBar_" + index);
+    // Set the width
+    progressBar.style.setProperty('width', width + '%');
+    // Set the text
+    progressBar.innerHTML = Math.round(count / totalVotes * 100) + '%';
   }
-=======
-	// TODO this page should be for a specific poll
-=======
-	// TODO: this page should be for a specific poll
-<<<<<<< HEAD
-	const poll_id = getPollID();
->>>>>>> ca7cd40... some minor poll_id refactor
-=======
-	poll_id = getPollID();
->>>>>>> 27cee40... fixed poll-id
-	const response = await fetch(`/poll/${poll_id}`);
-	poll = await response.json();
-	if (poll.message) throw new Error(poll.message);
-	return poll;
+  firstRender = false;
 }
 
-async function setup() {
-	createCanvas(400, 100);
-	await countVotes();
-	setInterval(countVotes, 500);
+const doTheFirstRender = (index, option, resultsDiv) => {
+  // Container div per option
+  let optionDiv = document.createElement('div');
+  optionDiv.classList.add('option');
 
-	radio = createRadio();
-	for (let i = 0; i < poll.options.length; i++) {
-		radio.option(i, poll.options[i]); // First arg is index, second arg is what is visible to user 
-	}
-	// radio.style('width', '180px'); // Change this for width of radio 
-	voteButton = createButton('vote');
-	voteButton.mousePressed(submitVote);
-}
+  // Add option text on a separate line, so it can get any length
+  let optionText = document.createElement('span');
 
-async function submitVote() {
-	let choice = radio.value(); // Choice is a number
-	// TODO: select poll id somehow => URL query parameters?
-	if (!isNaN(choice)) {
-		poll_id = getPollID();
-		let response = await fetch(`vote/${poll_id}/${choice}`);
-		let status = await response.json();
-		console.log(status);
-	} else {
-		console.log('no choice selected');
-	}
-}
+  optionText.innerHTML = option;
+  optionDiv.append(optionText);
+  resultsDiv.append(optionDiv);
 
-function draw() {
-	clear();
-	// background(255);
+  // Add option progress bar
+  let progressBar = document.createElement('div');
 
-	if (poll.options) {
-		let maxVotes = 0;
-		// Old code:
-		// for (let i = 0; i < poll.options.length; i++) {
-		// 	let count = poll.votes[i];
-		// 	maxVotes = max(count, maxVotes);
-		// }
-		// New code:
-		for (let count of poll.votes)
-			// For the vote count of each option do:
-			maxVotes = max(count, maxVotes);
-
-		let divisor = 1;
-
-		while (maxVotes / divisor > maxEmojis) divisor *= 5;
-
-		for (let i = 0; i < poll.options.length; i++) {
-			// Variables Breakdown: 
-			// i = index (before it was choice which was an alphabet)
-			// poll.options[i] = the actual option
-			// poll.votes[i]   = number of votes for this option    
-			let choice = poll.options[i];
-			let numEmojis = poll.votes[i] / divisor;
-			let x = 10;
-			let y = 20 + i * 20;
-
-			fill(0);
-			noStroke();
-			text("🚂".repeat(numEmojis), x, y, 10);
-			text(choice, x, y + 10);
-			// Resize as per requirements.
-		}
-	}
-}
->>>>>>> b2df93c... some cleanup in sketch.js
+  progressBar.id = 'progressBar_' + index;
+  progressBar.classList.add('progressBar');
+  resultsDiv.append(progressBar);
+};
