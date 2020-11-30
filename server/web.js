40bd7837b47f992a3b97e74b0e8943974c40ada4
Pieter De Schepper <pieter@netlog.com>
2020-11-30 15:56:10 +0100

    Document web routes

--- server/web.js
+++ server/web.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+//Index page to have an overview of active polls (and be ablo to manage them perhaps) - might need some password protection
 router.get("/", function (req, res) {
   res.render("index");
 });
 
+//Page to create a new poll
 router.get("/createPoll", function (req, res) {
   res.render("createPoll");
 });
 
+//Page to see the results of a poll
 router.get("/poll/:pollId", function (req, res) {
   res.render("poll");
 });
 
+//Page to add a vote to a poll
 router.get("/vote/:pollId", function (req, res) {
   res.render("vote");
 });
