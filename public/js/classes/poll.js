9349f5e14eaf038957cf11a3046867ab87cf91e8
Pieter De Schepper <pieter@netlog.com>
2020-12-06 12:02:08 +0100

    remove amount of votes on progress bars

--- public/js/classes/poll.js
+++ public/js/classes/poll.js
@@ -43,7 +43,7 @@ class Poll {
         pollDetails.totalVotes == 0
           ? 0
           : (count / pollDetails.totalVotes) * 100;
-      progressBar.html(`${count} votes (${Math.round(percent)}%)`);
+      progressBar.html(`${Math.round(percent)}%`);
     }
   }
 
