bc9161475b6cb57f5e27b797bdb56d35dd5b788a
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-12-06 01:42:56 -0700

    moved overlay to footer & made it a toggle

--- public/js/sketches/poll.js
+++ public/js/sketches/poll.js
@@ -5,21 +5,4 @@ async function setup() {
   noCanvas();
   poll = new Poll();
   poll.startPollingForVotes();
-  //regex for custom views (overlay)
-  const altViewRegEx = /(\?|\&)view=(overlay)($|\&)/;
-  //detect if there is a custom view
-  const isAltView = altViewRegEx.test(location.href);
-  //if there is a custom view
-  if (isAltView) {
-    //get the custom view name into a variable
-    const altView = altViewRegEx.exec(location.href)[2];
-    switch (altView) {
-      case "overlay": {
-        //set the background to semi-transperant white
-        document.body.style.background = "rgba(255,255,255,50%)"
-        //remove view links in overlay view
-        document.querySelector("div#views").remove();
-      } break;
-    }
-  }
 }
