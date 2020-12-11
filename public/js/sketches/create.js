7f53ca09650d30e522cf9af55278e40840030a36
Shaw Kai <66273574+ShawKai91@users.noreply.github.com>
2020-12-11 10:18:38 -0700

    combined footers + added gradients (#71)
    * updated readme + made poll_id
    
    * Update README.md
    
    * Update README.md
    
    * Move poll id into query parameters
    
    * update readme
    
    * fixed formating errors
    
    * trimed code
    
    + started work on restructer
    
    * added train for votesbar
    
    * tided up
    
    * + classnames to elements
    
    * update readme
    
    comments
    
    * it askes for poll_id
    
    * update readme
    
    * fafed aboot tring to figure it out
    
    * figured it out
    
    * updated readme
    
    * edits
    
    * Merge remote-tracking branch 'upstream/main' into main
    
    # Conflicts:
    #	README.md
    #	public/css/style.css
    #	public/js/script.js
    #	public/js/sketch.js
    #	server/index.js
    
    * tidyed up
    
    * rebased
    
    * + pull #63
    
    submitted by younesaassila
    
    * - some linebreaks
    
    * tweeked code
    
    timed some duplicate features
    
    * overlay toggle in footer?
    
    * moved overlay to footer & made it a toggle
    
    * chanded footer layout
    
    * + #73
    
    by elunico
    
    * clean up

--- public/js/sketches/create.js
+++ public/js/sketches/create.js
@@ -39,23 +39,17 @@ function setup() {
     });
     const { id } = await response.json();
 
-    console.log(id);
-
     textarea.hide();
     inputsDiv.hide();
     submit.hide();
 
     const voteLink = location.origin + "/vote/" + id;
     const pollLink = location.origin + "/poll/" + id;
-    const pollOverlayLink = location.origin + "/poll/" + id + "?view=overlay";
-    const pollNoGradientLink = location.origin + "/poll/" + id + "?view=nogradient";
     createDiv(`
     Poll Created Successfully.<br>
     Poll ID: ${id}<br>
     Poll Voting Link: <a href="${voteLink}">${voteLink}</a><br>
     Poll Results Link: <a href="${pollLink}">${pollLink}</a><br>
-    Poll Results Overlay Link: <a href="${pollOverlayLink}">${pollOverlayLink}</a><br>
-    Poll Results Link (No Gradients): <a href="${pollNoGradientLink}">${pollNoGradientLink}</a>
     `);
   });
 }
